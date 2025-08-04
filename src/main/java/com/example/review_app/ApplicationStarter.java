526eec9e4a1ec2dd0bc7d3e62d58fdb292fcf6c6
pedrohgdp <pedro.henrique.gp@outlook.com>
2025-08-04 12:27:23 -0300

    New things

--- src/main/java/com/example/review_app/ApplicationStarter.java
+++ src/main/java/com/example/review_app/ApplicationStarter.java
@@ -1,14 +1,19 @@
 package com.example.review_app;
 
 import com.example.review_app.classes.DataBaseController;
+import com.example.review_app.classes.Player;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectOutputStream;
 
 public class ApplicationStarter extends Application{
     DataBaseController db_controller = new DataBaseController();
+    Player player = new Player();
 
     @Override
     public void start(Stage stage) throws IOException {
@@ -18,6 +23,19 @@ public class ApplicationStarter extends Application{
         stage.setScene(scene);
         stage.show();
         db_controller.loadSubjectsFromDb();
+    }
+
+    @Override
+    public void init() throws IOException{
+        FileOutputStream fos = new FileOutputStream("src/main/resources/DB/PlayerDate.ser");
+        ObjectOutputStream oos = new ObjectOutputStream(fos);
+
+        oss.writeString("");
+    }
+
+    @Override
+    public void stop() throws IOException{
+
     }
 
     public static void main(String[] args) {
